50751036bc77c46b2ccd15ec295905711b693bca
Gavin Dreyer <gavindreyer1@gmail.com>
2019-10-29 14:52:42 -0700

    All working requests

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -12,8 +12,8 @@ router.get('/', (req, res) => {
         console.log(error);
         res.status(500).json({
             error: "The posts information could not be retrieved."
-        });
-    });
+        })
+    })
 })
 
 router.get('/:id', (req, res) => {
@@ -31,8 +31,27 @@ router.get('/:id', (req, res) => {
         console.log(error);
         res.status(500).json({
             error: "The post information could not be retrieved." 
-        });
-      });
+        })
+      })
+})
+
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id
+
+    Hubs.findPostComments(id)
+    .then(commentID => {
+        if(commentID.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.status(200).json(commentID)
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "The post information could not be retrieved." 
+        })
+      })
 })
 
 router.get('/:id/comments', (req, res) => {
@@ -50,8 +69,97 @@ router.get('/:id/comments', (req, res) => {
         console.log(error);
         res.status(500).json({
             error: "The post information could not be retrieved." 
-        });
-      });
+        })
+      })
+})
+
+router.post('/', (req, res) => {
+    const info = req.body
+    const { title, contents } = info
+
+    if(!title || !contents) res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+
+    Hubs.insert(info)
+    .then(newPost => {
+        res.status(201).json(newPost)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "There was an error while saving the post to the database"
+        })
+      })
+})
+
+router.post('/:id/comments', (req, res) => {
+    const id = req.params.id 
+    const info = req.body
+    const { text, post_id } = info
+    
+
+    if(!text) res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    console.log(id)
+
+    Hubs.insertComment(info)
+    .then(newPost => {
+        console.log(newPost)
+        if(newPost.length === 0){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.status(201).json(newPost)
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "There was an error while saving the post to the database"
+        })
+      })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+
+    Hubs.remove(id)
+    .then(count => {
+        console.log(count, id)
+        if(count === 0){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.status(200).json({ message: `Item with id ${id} deleted`})
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "The post could not be removed"
+        })
+      })
+
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const info = req.body
+    const { title, contents} = info
+
+    if(!title || !contents) res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+
+    Hubs.update(id, info)
+    .then(count => {
+        console.log(count)
+        if(count === 0){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.status(200).json(info)
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "The post information could not be modified."
+        })
+      })
 })
 
 module.exports = router;
